76a23d58750cf4daf89e66c97ba4e0ca1e14e860
Иван Попович <kooker22>
2020-06-23 23:26:05 +0300

    Updates

---
+++ precache-manifest.8712ab1a600e0fc9fe90f9241deec0cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2e37590da880ab1d69a7822d1508a7e",
+    "url": "/reactHW2/index.html"
+  },
+  {
+    "revision": "31eba966018cc4feb518",
+    "url": "/reactHW2/static/css/2.4b502e0b.chunk.css"
+  },
+  {
+    "revision": "31eba966018cc4feb518",
+    "url": "/reactHW2/static/js/2.21178e22.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactHW2/static/js/2.21178e22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42cbeecb155c9c872750",
+    "url": "/reactHW2/static/js/main.2336dfca.chunk.js"
+  },
+  {
+    "revision": "692c7fa8cbe26bb6499b",
+    "url": "/reactHW2/static/js/runtime-main.9ed83528.js"
+  }
+]);
